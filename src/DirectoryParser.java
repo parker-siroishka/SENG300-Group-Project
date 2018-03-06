32c81648c2d04fcf395da25a898c27c211aa9b87
Parker Siroishka <parkersir10@hotmail.com>
2018-03-06 14:22:20 -0700

    Class Declarations are counted and displayed
    Counts and displays Class Declarations in directory. Have not implemented references

--- src/DirectoryParser.java
+++ src/DirectoryParser.java
@@ -7,20 +7,27 @@ import java.io.File;
 
 import org.eclipse.jdt.core.dom.*;
 
-
-
-
-
 public class DirectoryParser {
 	
-	public static String convertFilesToString(String directoryPath)throws IOException {
+	int counter = 0;
+	TypeDeclaration tempNode;
+	
+	/**
+	 * Iterates through all files in directory 
+	 * 
+	 * @param directoryPath - location to directory for parsing
+	 * @return String - String of java code in directory
+	 * @throws IOException - If invalid directoryPath
+	 */
+	public static String convertFilesToString(String directoryPath)throws IOException, FileNotFoundException {
 		String target_dir = directoryPath;
 		StringBuilder sb = new StringBuilder();
 		File dir = new File(target_dir);
 		File[] files = dir.listFiles();
 		
 		for (File f : files) {
-			if(f.isFile()) {
+			
+			if(f.isFile() && f.getName().toLowerCase().endsWith(".java")) {
 				BufferedReader inputStream = null;
 				
 				try {
@@ -42,37 +49,50 @@ public class DirectoryParser {
 		return sb.toString();
 	}
 	
-	public static void main(String[] args) throws IOException {
-		Scanner kb = new Scanner(System.in);
-		
+	public int getClassDeclarationCount(String pathname) throws IOException {
+		char[] directoryContent = convertFilesToString(pathname).toCharArray();
 		ASTParser parser = ASTParser.newParser(AST.JLS8);
+		parser.setSource(directoryContent);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setResolveBindings(true);
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 		
+		cu.accept(new ASTVisitor() {
+			
+			public boolean visit(TypeDeclaration node) {
+				counter++;
+				return false;
+			}
+		});
+		return counter;
+	}
+	
+	public int parseSpecifiedType(int type, String pathname) throws IOException {
+		if(type == 1) {
+			System.out.println("\nClass Type." + "Declarations: " + this.getClassDeclarationCount(pathname) + 
+					" References: " + "N/A");
+		}else if (type == 2) {
+			System.out.println("Interface Declaration not implemented yet");
+		}else if (type == 3) {
+			System.out.println("Enum Declaration not implemented yet");
+		}else {
+			System.out.println("Annotation Type Declaration not implemented yet");
+		}
+		return 0;
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		DirectoryParser dp = new DirectoryParser();
+		
+		Scanner kb = new Scanner(System.in);
 		System.out.print("Enter directory pathname: ");
 		String pathname = kb.next();
-		
-		char[] directoryContent = convertFilesToString(pathname).toCharArray();
-		
-		parser.setSource(directoryContent);
-		
-		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-		
-		
-		
-		//System.out.print("Enter name of Java type to count (Ex: 'SIMPLE_NAME', 'TRY_STATEMENT'); ");
-		//String javaType = kb.next();
-
-			cu.accept(new ASTVisitor() {
-				
-				public boolean visit(VariableDeclarationFragment node) {
-					System.out.println("DECLARATION FOUND");
-					return true;
-					
-					
-					
-				}
-			});
-		
-		//System.out.println("Variable Declarations: " + declarations);
-		
+		System.out.print("Count (1)Class Declarations, \n      "
+				+ "(2)Interface Declarations, \n      "
+				+ "(3)Enum Declaration, or \n      "
+				+ "(4) Annotation Type Declaration: ");
+		int specifiedType = kb.nextInt();
+		dp.parseSpecifiedType(specifiedType, pathname);
 	}
 }
